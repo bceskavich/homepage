18a71c7c957544332b74301ed0237755c63f9263
Billy Ceskavich <bceskavich@gmail.com>
2014-05-29 12:03:34 -0700

    Changes to skills orientation

--- js/index.js
+++ js/index.js
@@ -1,53 +1,47 @@
 function renderSkills(){
-    var margin = {top: 20, bottom: 30}
         width = $("#skills").width(),
-        height = 300;
+        barHeight = 35;
 
-    var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-
-    var yScale = d3.scale.linear().range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom");
+    var xScale = d3.scale.linear().range([0, width]);
 
     var graph = d3.select("#skills")
         .append("svg")
         .attr("id", "chart")
         .attr("width", width)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(0," + margin.top + ")");
 
     d3.csv("../data/skills.csv", type, function(error, data){
-        xScale.domain(data.map(function(d){
-            return d.name;
-        }));
-        yScale.domain([0, d3.max(data, function(d){
+        xScale.domain([0, d3.max(data, function(d){
             return d.level;
         })]);
+
+        graph.attr("height", barHeight * data.length);
 
         var bar = graph.selectAll("g")
             .data(data)
             .enter()
             .append("g")
             .attr("transform", function(d, i){
-                return "translate(" + xScale(d.name) + ",0)";
+                return "translate(0," + i * barHeight + ")";
             });
 
         bar.append("rect")
-            .attr("y", function(d){
-                return yScale(d.level);
+            .attr("transform", function(d){
+                return "translate(" + (width - xScale(d.level)) + ",0)";
             })
-            .attr("height", function(d){
-                return height - yScale(d.level);
-            })
-            .attr("width", xScale.rangeBand());
+            .attr("height", barHeight - 5)
+            .attr("width", function(d){
+                return xScale(d.level);
+            });
 
-        graph.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", "translate(0," + (height - 25) + ")")
-            .call(xAxis);
+        bar.append("text")
+            .attr("y", barHeight / 2)
+            .attr("x", function(d){
+                return (width + 10) - xScale(d.level);
+            })
+            .attr("dy", ".25em")
+            .text(function(d){
+                return d.name;
+            });
     });
 }
 
