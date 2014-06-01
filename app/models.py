386526896a7cedcf37cacb6b64f7ef2d7da8839a
Billy Ceskavich <bceskavich@gmail.com>
2014-06-01 16:56:19 -0700

    Initial scaffolding for User & Work db models.

--- app/models.py
+++ app/models.py
@@ -4,4 +4,26 @@ from app import app, db
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True)
-    password =
+    password = db.Column(db.String(64))
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+class Work(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(256), index = True)
+    description = db.Column(db.Text)
+    url = db.Column(db.String(256))
+
